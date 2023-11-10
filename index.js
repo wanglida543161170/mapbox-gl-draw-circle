e803fd4e6a75ceeeadd230318a0c777c494a90b9
wangjiuchun <wangjiuchun@jiachengnet.com>
2023-11-10 10:53:12 +0800

    fix: update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-export const CircleMode = require('./lib/modes/CircleMode');
-export const DragCircleMode = require('./lib/modes/DragCircleMode');
-export const DirectMode = require('./lib/modes/DirectModeOverride');
-export const SimpleSelectMode = require('./lib/modes/SimpleSelectModeOverride');
+import CircleMode from './lib/modes/CircleMode';
+import DragCircleMode from './lib/modes/DragCircleMode';
+import DirectMode from './lib/modes/DirectModeOverride';
+import SimpleSelectMode from './lib/modes/SimpleSelectModeOverride';
+
+export {CircleMode, DirectMode, DragCircleMode, SimpleSelectMode}
